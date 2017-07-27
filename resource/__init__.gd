43634a02e56d48aaf1e4d93861a0474345ecb42b
Geequlim <geequlim@gmail.com>
2017-07-27 09:22:58 +0800

    Add image tools Add text file tools

--- resource/__init__.gd
+++ resource/__init__.gd
@@ -27,7 +27,33 @@
 ##################################################################################
 
 tool
-const AutoLayoutControl = preload("AutoLayoutControl.gd")
-const Layout = preload("Layout.gd")
-const Menu = preload("Menu.gd")
-const Window = preload("Window.gd")
+const image = preload("image.gd")
+
+# Load text file content as string  
+# - - -  
+# **parameter**  
+# * [path: String] file path to load  
+# - - -  
+# **Return**  
+# * String file content
+static func load_text_content(path):
+    var file = File.new()
+    if OK == file.open(path, File.READ):
+        return file.get_as_text()
+    return ""
+
+# Save text into file  
+# - - -  
+# **parameter**  
+# * [text: String] The text content to save  
+# * [path: String] The file path to save with  
+# - - -  
+# **Return**  
+# * in : OK | error code  
+static func save_text_content(text, path):
+    var file = File.new()
+    var err = file.open(path, File.WRITE)
+    if OK == err:
+        file.store_string(text)
+    return err
+
--- resource/image.gd
+++ resource/image.gd
@@ -27,7 +27,38 @@
 ##################################################################################
 
 tool
-const AutoLayoutControl = preload("AutoLayoutControl.gd")
-const Layout = preload("Layout.gd")
-const Menu = preload("Menu.gd")
-const Window = preload("Window.gd")
+
+# Load image texture from external path  
+# - - -  
+# **Parameters**  
+# * [path: String] The absolute image path  
+# - - -  
+# **Returns**  
+# * ImageTexture | null
+static func load_external_image_texture(path):
+    var img = Image.new()
+    if OK == img.load(path):
+        var tex = ImageTexture.new()
+        tex.create_from_image(img)
+        return tex
+    return null
+
+# Save image into png file  
+# - - -  
+# **Parameters**  
+# * [texture: Image|ImageTexture] The image or texture to save  
+# * [path: String] The file path to save with  
+# - - -  
+# **Returns**  
+# *  [int] OK | error code
+static func save_image_texture(texture, path):
+    if texture == null or typeof(texture) != TYPE_OBJECT or typeof(path) != TYPE_STRING:
+        return ERR_INVALID_PARAMETER
+    var img = null
+    if texture is Image:
+        img = texture
+    elif texture is ImageTexture:
+        img = texture.get_data()
+    if img != null:
+        return img.save_png(path)
+    return ERR_INVALID_DATA
