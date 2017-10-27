f2e34e253569f72d129767bb7288b410e0953bbc
Geequlim <geequlim@gmail.com>
2017-10-27 14:26:37 +0800

    InstanceManager: add weakref support Add @ word in the flag prefix contents

--- utils/InstanceManager.gd
+++ utils/InstanceManager.gd
@@ -29,10 +29,16 @@
 tool
 const json = preload('json.gd')
 
-const TYPE_OBJECT_PREFIX     = "Object:"
-const LEN_TYPE_OBJECT_PREFIX = 7
-const TYPE_ATOMIC_PREFIX     = "Atomic:"
-const LEN_TYPE_ATOMIC_PREFIX = 7
+const TYPE_ATOMIC_PREFIX     = "@Atomic:"
+const LEN_TYPE_ATOMIC_PREFIX = 8
+
+const TYPE_OBJECT_PREFIX     = "@Object:"
+const LEN_TYPE_OBJECT_PREFIX = 8
+
+const TYPE_REF_PREFIX		 = "@Reference:"
+const LEN_TYPE_REF_PREFIX    = 11
+
+const MEATA_NAME_INST_UID 	 = "@InstanceID"
 
 # Atomic types that json doesn't support
 const SERIALIZED_ATOMIC_TYPES = [
@@ -151,31 +157,35 @@ func _serialize(inst):
 	var ret = inst
 	if typeof(inst) in SERIALIZED_ATOMIC_TYPES:
 		ret = str(TYPE_ATOMIC_PREFIX, var2str(inst))
-	elif typeof(inst) == TYPE_OBJECT and inst.get_script() != null:
-		var uid = 0
-		var is_replace = false
-		var InstID = inst.get_instance_id()
-		if not _instance_uid_map.has(InstID):
-			uid = _max_instance_uid
-			if inst.has_meta("@InstanceID"):
-				uid = inst.get_meta("@InstanceID")
-				if self.pool.has(uid):
-					is_replace = true
-					var old_inst_id = _uid_instance_map[uid]
-					_instance_uid_map[old_inst_id] = _max_instance_uid
-					_uid_instance_map[_max_instance_uid] = old_inst_id
-					self.pool[_max_instance_uid] = self.pool[uid]
-			_instance_uid_map[InstID] = uid
-			_uid_instance_map[uid]    = InstID
-			_max_instance_uid += 1
-		else:
-			uid = _instance_uid_map[InstID]
-		if is_replace or (not self.pool.has(uid)):
-			var dict = inst2dict(inst)
-			self.pool[uid] = dict
-			for key in dict:
-				dict[key] = _serialize(dict[key])
-		ret = str(TYPE_OBJECT_PREFIX, uid)
+	elif typeof(inst) == TYPE_OBJECT:
+		if inst is WeakRef:
+			var obj = inst.get_ref()
+			ret = _serialize(obj).replace(TYPE_OBJECT_PREFIX, TYPE_REF_PREFIX) if obj != null else null
+		elif inst.get_script() != null:
+			var uid = 0
+			var is_replace = false
+			var InstID = inst.get_instance_id()
+			if not _instance_uid_map.has(InstID):
+				uid = _max_instance_uid
+				if inst.has_meta(MEATA_NAME_INST_UID):
+					uid = inst.get_meta(MEATA_NAME_INST_UID)
+					if self.pool.has(uid):
+						is_replace = true
+						var old_inst_id = _uid_instance_map[uid]
+						_instance_uid_map[old_inst_id] = _max_instance_uid
+						_uid_instance_map[_max_instance_uid] = old_inst_id
+						self.pool[_max_instance_uid] = self.pool[uid]
+				_instance_uid_map[InstID] = uid
+				_uid_instance_map[uid]    = InstID
+				_max_instance_uid += 1
+			else:
+				uid = _instance_uid_map[InstID]
+			if is_replace or (not self.pool.has(uid)):
+				var dict = inst2dict(inst)
+				self.pool[uid] = dict
+				for key in dict:
+					dict[key] = _serialize(dict[key])
+			ret = str(TYPE_OBJECT_PREFIX, uid)
 	elif typeof(inst) == TYPE_ARRAY:
 		ret = []
 		for ele in inst:
@@ -208,7 +218,7 @@ func _unserialize(any, rawPool):
 		if any.has("@path") and any.has("@subpath"):
 			ret = dict2inst(any)
 			if typeof(ret) == TYPE_OBJECT:
-				ret.set_meta("@InstanceID", _parsing_instance_uid)
+				ret.set_meta(MEATA_NAME_INST_UID, _parsing_instance_uid)
 	elif typeof(any) == TYPE_ARRAY:
 		ret = []
 		for ele in any:
@@ -222,10 +232,15 @@ func _setup_references(inst):
 			var prop = inst.get(propDesc.name)
 			if typeof(prop) in [TYPE_STRING, TYPE_ARRAY, TYPE_DICTIONARY]:
 				inst.set(propDesc.name, _setup_references(prop))
-	elif typeof(inst) == TYPE_STRING and inst.begins_with(TYPE_OBJECT_PREFIX):
-		var id = int(inst.substr(LEN_TYPE_OBJECT_PREFIX, inst.length()))
-		if self.pool.has(id):
-			ret = self.pool[id]
+	elif typeof(inst) == TYPE_STRING:
+		if inst.begins_with(TYPE_OBJECT_PREFIX):
+			var id = int(inst.substr(LEN_TYPE_OBJECT_PREFIX, inst.length()))
+			if self.pool.has(id):
+				ret = self.pool[id]
+		elif inst.begins_with(TYPE_REF_PREFIX):
+			var id = int(inst.substr(LEN_TYPE_REF_PREFIX, inst.length()))
+			if self.pool.has(id):
+				ret = weakref(self.pool[id])
 	elif typeof(inst) == TYPE_ARRAY:
 		ret = []
 		for ele in inst:
