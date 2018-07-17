16411721a0bdcf704eb57ba99c4fe841b3c2da81
geequlim <geequlim@gmail.com>
2018-07-18 00:18:41 +0800

    [http] Fix node doesn't freed after request over

--- utils/http.gd
+++ utils/http.gd
@@ -48,6 +48,7 @@ var request_nodes = {}
 # * Return OK or other error code
 func request(url, method = HTTPClient.METHOD_GET, headers=[], body = ""):
     var rq = HTTPRequest.new()
+    # rq.name = url.replace("/", '\\')
     rq.use_threads = true
     rq.connect("request_completed", self, "_on_request_completed", [url])
     add_child(rq)
@@ -78,25 +79,43 @@ func cancel_request(url):
 # *Returns* Variant  
 # * Return the resolved result or the body if not resolved
 static func resolve_respose_body(headers, body):
-    if typeof(body) == TYPE_RAW_ARRAY and typeof(headers) == TYPE_STRING_ARRAY:
+    if typeof(body) == TYPE_RAW_ARRAY:
+        var type = get_content_type(headers)
+        match type:
+            'image/jpeg', 'application/x-jpg':
+                var img = Image.new()
+                img.load_jpg_from_buffer(body)
+                return img
+            'image/png', 'application/x-png':
+                var img = Image.new()
+                img.load_png_from_buffer(body)
+                return img
+            'image/webp':
+                var img = Image.new()
+                img.load_webp_from_buffer(body)
+                return img
+            'application/json':
+                return parse_json(body.get_string_from_utf8())
+        if type.begins_with("text/"):
+            return body.get_string_from_utf8()
+    return body
+
+# Get content type from the response headers  
+# - - - - - - - - - -  
+# *Parameters*  
+# * [headers: PoolStringArray] headers  
+# - - - - - - - - - -  
+# *Returns* String  
+# * The content type with lower case or an empty string if parse faild
+static func get_content_type(headers):
+    var type = ''
+    if typeof(headers) in [TYPE_STRING_ARRAY, TYPE_ARRAY]:
         for item in headers:
             item = item.to_lower()
             if item.begins_with('content-type:'):
-                var type = item.substr(len('content-type:'), len(item)).strip_edges()
-                match type:
-                    'image/jpeg', 'application/x-jpg':
-                        var img = Image.new()
-                        img.load_jpg_from_buffer(body)
-                        return img
-                    'image/png', 'application/x-png':
-                        var img = Image.new()
-                        img.load_png_from_buffer(body)
-                        return img
-                    'application/json':
-                        return parse_json(body.get_string_from_utf8())
-                if type.begins_with("text/"):
-                    return body.get_string_from_utf8()
-    return body
+                type = item.substr(len('content-type:'), len(item)).strip_edges()
+                break
+    return type
 
 
 func _process(delta):
@@ -104,6 +123,9 @@ func _process(delta):
         var rq = request_nodes[url]
         if rq.get_http_client_status() == HTTPClient.STATUS_BODY:
             emit_signal('request_progress', url, rq.get_downloaded_bytes(), rq.get_body_size())
+    for node in get_children():
+        if not node in request_nodes.values():
+            node.queue_free()
 
 func _on_request_completed(result, response_code, headers, body, url):
     if not url in request_nodes: return
@@ -113,6 +135,6 @@ func _on_request_completed(result, response_code, headers, body, url):
         emit_signal('request_completed', url, headers, body)
     else:
         emit_signal('request_failed', url, result, response_code)
-    if url in request_nodes:
-        request_nodes[url].queue_free()
-        request_nodes.erase(url)
+    # remove the request node
+    request_nodes[url].queue_free()
+    request_nodes.erase(url)
