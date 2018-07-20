3bdd3becd95b071484bcf3cc77cd59a5c3e2406e
geequlim <geequlim@gmail.com>
2018-07-21 00:29:18 +0800

    Add editor tool menu to open user data directory

---
+++ editor/tools.gd
@@ -0,0 +1,13 @@
+tool
+const MENUS = {
+	"Open user dir": 0,
+}
+
+func initialize(plugin):
+	for key in MENUS:
+		plugin.add_tool_menu_item(key, self, '_on_menu_pressed', MENUS[key])
+
+func _on_menu_pressed(action):
+	match action:
+		0:
+			OS.shell_open(str('file://', OS.get_user_data_dir()))
--- plugin.gd
+++ plugin.gd
@@ -28,6 +28,7 @@
 
 tool
 extends EditorPlugin
+var tools = preload("editor/tools.gd").new()
 
 func _enter_tree():
 	add_custom_type("AsyncHttpTextureRect", "TextureRect", preload("scene/gui/AsyncHttpTextutreRect.gd"), null)
@@ -35,6 +36,9 @@ func _enter_tree():
 	add_custom_type("ItemListEnhanced", "ItemList", preload("scene/gui/ItemListEnhanced.gd"), null)
 	add_custom_type("InfinityList", "ScrollContainer", preload("scene/gui/InfinityList.gd"), null)
 	add_custom_type("GridBackground", "TextureRect", preload("scene/gui/GridBackground.gd"), null)
+
+func _ready():
+	tools.initialize(self)
 
 func _exit_tree():
 	remove_custom_type("AsyncHttpTextureRect")
