2c65bc8f0e1f0286120074f0cf8e34db0062246e
Geequlim <geequlim@gmail.com>
2019-05-27 16:04:07 +0800

    implemens basic Inferface check method

--- utils/utils.gd
+++ utils/utils.gd
@@ -1,11 +1,29 @@
-static func implements(obj, interface= {}):
+
+# Check does an object has all of script methods of target class  
+# - - - - - - - - - -  
+# *Parameters*  
+# * [obj:Object] The object to check  
+# * [interface:GDScript] The interface to check with 
+# - - - - - - - - - -  
+# *Returns* bool  
+static func implements(obj: Object, interface:GDScript) -> bool:
 	if obj == null or interface == null:
 		return false
-	if not (typeof(obj) == TYPE_OBJECT):
+	if typeof(obj) != TYPE_OBJECT:
 		return false
-	if typeof(interface) == TYPE_OBJECT and obj is interface:
+	if obj is interface:
 		return true
-	return false
+	var interface_obj = interface.new()
+	var required_methods = []
+	for m in interface_obj.get_method_list():
+		if m.flags & METHOD_FLAG_FROM_SCRIPT:
+			required_methods.append(m.name)
+	if not interface_obj is Reference:
+		interface_obj.free()
+	for mid in required_methods:
+		if not obj.has_method(mid):
+			return false
+	return true
 
 # Format the time duration to a text to display  
 # - - - - - - - - - -  
