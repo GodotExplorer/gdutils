6c5bca90fdcf50a8ea2df71a8248e8d6bcbef767
Geequlim <geequlim@gmail.com>
2019-06-03 11:27:45 +0800

    Add ObjectPool to utils

--- utils/ObjectPool.gd
+++ utils/ObjectPool.gd
@@ -1,6 +1,4 @@
 ##################################################################################
-#                        Tool generated DO NOT modify                            #
-##################################################################################
 #                            This file is part of                                #
 #                                GodotExplorer                                   #
 #                       https://github.com/GodotExplorer                         #
@@ -27,61 +25,85 @@
 ##################################################################################
 
 tool
+var objects = []
+var _using_objects = {}
 
-const AsyncTaskQueue = preload("AsyncTaskQueue.gd")
-const InstanceManager = preload("InstanceManager.gd")
-const ObjectPool = preload("ObjectPool.gd")
-const csv = preload("csv.gd")
-const http = preload("http.gd")
-const json = preload("json.gd")
-const uuid = preload("uuid.gd")
+# Pool size
+var size = 0 setget resize, get_size
+func get_size() -> int:
+	return objects.size()
 
+# Using object count in the pool
+var using_count setget , get_using_count
+func get_using_count() -> int:
+	var count = 0
+	for i in range(objects.size()):
+		if _using_objects[i]:
+			count += 1
+	return count
 
-# Check does an object has all of script methods of target class  
-# - - - - - - - - - -  
-# *Parameters*  
-# * [obj:Object] The object to check  
-# * [interface:GDScript] The interface to check with 
-# - - - - - - - - - -  
-# *Returns* bool  
-static func implements(obj: Object, interface:GDScript) -> bool:
-	if obj == null or interface == null:
-		return false
-	if typeof(obj) != TYPE_OBJECT:
-		return false
-	if obj is interface:
-		return true
-	var interface_obj = interface.new()
-	var required_methods = []
-	for m in interface_obj.get_method_list():
-		if m.flags & METHOD_FLAG_FROM_SCRIPT:
-			required_methods.append(m.name)
-	if not interface_obj is Reference:
-		interface_obj.free()
-	for mid in required_methods:
-		if not obj.has_method(mid):
-			return false
-	return true
+# Set the pool size
+func resize(v: int):
+	if v >= 0:
+		var last_size = objects.size()
+		if v > objects.size():
+			objects.resize(v)
+			for i in range(last_size, v):
+				objects[i] = create_object()
+				_using_objects[i] = false
+		elif v < last_size:
+			for i in range(v, last_size):
+				var obj = objects[i]
+				destroy_object(obj)
+				_using_objects[i] = false
+			objects.resize(v)
 
-# Format the time duration to a text to display  
-# - - - - - - - - - -  
-# *Parameters*  
-# * [seconds: float] The time duration in seconds  
-# * [always_show_hours: bool = `false`] Show hours even if the time is less than one hour
-# - - - - - - - - - -  
-# *Returns* String  
-# * The time duration as `hh:mm:ss`
-static func format_time_duration(second: float, always_show_hours = false) -> String:
-	var h = floor(second / (60.0 * 60.0))
-	var m = floor((second - h * 60.0 * 60.0) / 60.0)
-	var s = int(second) % 60
-	var ret = ""
-	if h > 0 or always_show_hours:
-		if h < 10: ret += "0"
-		ret += str(h, ":")
-	if m < 10: ret += "0"
-	ret += str(m, ":")
-	if s < 10: ret += "0"
-	ret += str(s)
-	return ret
+# Get an object from the pool
+func get_object() -> Object:
+	for i in range(objects.size()):
+		if not _using_objects[i]:
+			_using_objects[i] = true
+			return objects[i]
+	var idx = objects.size()
+	resize(idx + 1)
+	_using_objects[idx] = true
+	return objects[idx]
 
+# Release an object to the pool  
+# The object must be returned from `get_object` method 
+func release(p_object: Object):
+	var idx = objects.find(p_object)
+	if idx != -1:
+		if _using_objects[idx]:
+			release_object(p_object)
+			_using_objects[idx] = false
+
+# Release all of the objects in the pool  
+# Make all of the objects is usable  
+# This method won't destroy objects  
+func clear():
+	for i in range(objects.size()):
+		if _using_objects[i]:
+			release_object(objects[i])
+			_using_objects[i] = false
+
+# Destroy all of the objects in the pool
+func destroy():
+	clear()
+	for obj in objects:
+		destroy_object(obj)
+	resize(0)
+	_using_objects = {}
+
+# The method to override to create object for the pool
+func create_object() -> Object:
+	return null
+
+# Call on an object is release(return back) to the pool
+func release_object(p_object: Object):
+	pass
+
+# The method to override to destroy object for the pool
+func destroy_object(p_object: Object):
+	if not p_object is Reference:
+		p_object.free()
